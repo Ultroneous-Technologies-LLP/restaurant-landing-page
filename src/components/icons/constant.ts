bd09279dbc38ad823eba2c6b095249300219cd17
Vijaydev19 <dev19.ultroneous@gmail.com>
2025-11-10 15:12:17 +0530

    fix: refactor icon components for improved consistency and maintainability

---
+++ src/components/icons/constant.ts
@@ -0,0 +1,11 @@
+export const ICON_SIZE_0 = 0;
+export const ICON_SIZE_10 = 10;
+export const ICON_SIZE_12 = 12;
+export const ICON_SIZE_18 = 18;
+export const ICON_SIZE_20 = 20;
+export const ICON_SIZE_22 = 22;
+export const ICON_SIZE_23 = 23;
+export const ICON_SIZE_24 = 24;
+export const ICON_SIZE_25 = 25;
+export const ICON_SIZE_26 = 26;
+export const ICON_SIZE_28 = 28;
