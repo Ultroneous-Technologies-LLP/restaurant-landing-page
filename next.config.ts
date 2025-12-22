04e7eaa0d559557f1584e0a0da691454ad117192
Vijaydev19 <dev19.ultroneous@gmail.com>
2025-12-22 12:43:15 +0530

    fix: required changes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',   
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
